cbd12bfed5a1294be7f89b4863a2403ca23ed138
djedr <djedr@users.noreply.github.com>
2016-02-09 14:47:29 +0100

    Delete raytracer.task2.js

--- renderer/raytracer.task2.js
+++
@@ -1,7 +0,0 @@
-function initialize() {
-    var canvas = document.getElementById('front_buffer');
-    var raytracer = new mod.raytracer(new mod.canvas(canvas));
-    raytracer.render();
-}
-
-document.addEventListener('DOMContentLoaded', initialize, false);
